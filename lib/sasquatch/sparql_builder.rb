1a069fb4d5eab6897a2541305f9a26b21d69d034
Ross Singer <rossfsinger@gmail.com>
2011-03-04 14:01:37 -0500

    Some fixes for CONSTRUCT queries and DESCRIBE queries with only OPTIONAL clauses

--- lib/sasquatch/sparql_builder.rb
+++ lib/sasquatch/sparql_builder.rb
@@ -18,7 +18,7 @@ module Sasquatch
     end
     
     def set_variables(*vars)
-      @variables = vars.flatten
+      @variables = vars
       self
     end
     def describe!(graph=:default)
@@ -28,7 +28,7 @@ module Sasquatch
     
     def describe
       @form = :describe
-      @values = @variables.map { |var|
+      @values = *@variables.flatten.map { |var|
         [var, var.is_a?(RDF::URI) ? var : RDF::Query::Variable.new(var)]
       }      
       self
@@ -41,7 +41,7 @@ module Sasquatch
     
     def select
       @form = :select
-      @values = @variables.map { |var| [var, RDF::Query::Variable.new(var)] }
+      @values = @variables.flatten.map { |var| [var, RDF::Query::Variable.new(var)] }
       self
     end
     
@@ -62,12 +62,59 @@ module Sasquatch
     
     def construct
       @form = :construct
-      options[:template] = build_patterns(@variables)
+      options[:template] = build_patterns(*@variables)
       self
     end
     
     def execute(graph)
       @store.send(:"sparql_#{@form}", self.to_s, graph)
     end
+    
+    ##
+    # Returns the string representation of this query.
+    #
+    # @return [String]
+    def to_s
+      buffer = [form.to_s.upcase]
+      case form
+        when :select, :describe
+          buffer << 'DISTINCT' if options[:distinct]
+          buffer << 'REDUCED' if options[:reduced]
+          buffer << (values.empty? ? '*' : values.map { |v| serialize_value(v[1]) }.join(' '))
+        when :construct
+          buffer << '{'
+          buffer += serialize_patterns(options[:template])
+          buffer << '}'
+      end
+
+      buffer << "FROM #{serialize_value(options[:from])}" if options[:from]
+
+      unless (patterns.empty? && (options[:optionals].nil? || options[:optionals].empty?)) && form == :describe
+        buffer << 'WHERE {'
+        buffer += serialize_patterns(patterns)
+        if options[:optionals]
+          options[:optionals].each do |patterns|
+            buffer << 'OPTIONAL {'
+            buffer += serialize_patterns(patterns)
+            buffer << '}'
+          end
+        end
+        if options[:filters]
+          buffer += options[:filters].map { |filter| "FILTER(#{filter})" }
+        end
+        buffer << '}'
+      end
+
+      if options[:order_by]
+        buffer << 'ORDER BY'
+        buffer += options[:order_by].map { |var| var.is_a?(String) ? var : "?#{var}" }
+      end
+
+      buffer << "OFFSET #{options[:offset]}" if options[:offset]
+      buffer << "LIMIT #{options[:limit]}" if options[:limit]
+      options[:prefixes].reverse.each {|e| buffer.unshift("PREFIX #{e}") } if options[:prefixes]
+
+      buffer.join(' ')
+    end    
   end
 end
